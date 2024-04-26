b593988f2e158c5382d0b3ca9409bbce1bf41a63
타래 <85001878+meowTarae@users.noreply.github.com>
2024-04-26 15:23:10 +0900

    Feat: Router에 적용된 suspense에 맞춰 DetailPage 내부 loading 제거

--- src/pages/DetailPage/DetailPage.tsx
+++ src/pages/DetailPage/DetailPage.tsx
@@ -5,28 +5,17 @@ import { PostContainer } from './PostContainer';
 import { postIdContext } from './components/DetailPostContext';
 import { useGetPostDetail } from '@/hooks/query/usePost';
 import { StSideMarginWrapper } from '@/style/StSideMarginWrapper';
-import { Spinner } from '@common/index';
 
 export const DetailPage = () => {
   const { id } = useParams();
   const navigate = useNavigate();
-  const { data, isLoading, isError } = useGetPostDetail(id || '');
+  const { data, isError } = useGetPostDetail(id || '');
   const response = data.data;
 
   if (isError) {
     alert('API로부터 데이터를 받아올 때 에러가 발생했습니다.');
     navigate('/');
   }
-
-  // Todo: Suspense 작업 시 수정 예정
-  if (isLoading)
-    return (
-      <>
-        <StSpinnerWrapper>
-          <Spinner size={36} />
-        </StSpinnerWrapper>
-      </>
-    );
 
   return (
     <StSideMarginWrapper>
@@ -46,11 +35,4 @@ export const DetailPage = () => {
 
 const StDetailContainer = styled.div`
   padding: 32px;
-`;
-
-const StSpinnerWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 50vh;
 `;
